140279ce81bdabec07bfe03d16105bc97c2c83a3
mayaaakotaka <klyster2006@gmail.com>
2023-06-23 16:09:28 -0300

    foi????

--- src/main/java/ifpr/pgua/eic/exemplo/Launcher.java
+++ src/main/java/ifpr/pgua/eic/exemplo/Launcher.java
@@ -2,6 +2,6 @@ package ifpr.pgua.eic.exemplo;
 
 public class Launcher {
     public static void main(String[] args) {
-        App.launch(App.class, args);
+        App.launch(App.class, args); 
     }
 }
